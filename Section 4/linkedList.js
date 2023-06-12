f9d3b417c3d149258b06ed6cfc5219b8af77fc66
hassan <hassan.elouardy06@gmail.com>
2023-06-12 12:34:30 +0100

    Add reverse method

--- Section 4/linkedList.js
+++ Section 4/linkedList.js
@@ -142,13 +142,28 @@ class  LinkedList {
 
         const before = this.get(index-1);
         const temp = before.next;
-        
+
         before.next = temp.next;
         temp.next = null;
         this.length--;
         return temp;
     }
 
+    reverse() {
+        let temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+        let next = temp.next;
+        let prev = null;
+
+        for(let i = 0 ; i < this.length ; i++) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+        return this;
+    }
 }
 
 let myLinkedList = new LinkedList(4);
