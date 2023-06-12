6fde29cd42bc891e4bce200e7fb9611357d5d705
hassan <hassan.elouardy06@gmail.com>
2023-06-12 12:13:39 +0100

    Add remove method

--- Section 4/linkedList.js
+++ Section 4/linkedList.js
@@ -134,6 +134,21 @@ class  LinkedList {
 
     }
 
+    remove(index) {
+        //Remove Node
+        if(index === 0) return this.shift();
+        if(index === this.length - 1) return this.pop();
+        if(index < 0 || index >= this.length ) return undefined;
+
+        const before = this.get(index-1);
+        const temp = before.next;
+        
+        before.next = temp.next;
+        temp.next = null;
+        this.length--;
+        return temp;
+    }
+
 }
 
 let myLinkedList = new LinkedList(4);
