f48369aaba4ebfb2baf323de04b2a0147c9267f9
hassan <hassan.elouardy06@gmail.com>
2023-06-17 01:55:58 +0100

    Create set method

--- Section 7 Doubly Linked Lists/dll.js
+++ Section 7 Doubly Linked Lists/dll.js
@@ -96,6 +96,16 @@ class DoublyLinkedList {
     }
     return temp
   }
+
+  //set method
+  set(index, value) {
+    let temp = this.get(index);
+    if(temp) {
+      temp.value = value;
+      return true
+    }
+    return false;
+  }
 }
 
 //Example
