b62e87419ececfa1bf623ae9d69f81ae29f23c97
hassan <hassan.elouardy06@gmail.com>
2023-06-17 02:41:28 +0100

    Add remove method

--- Section 7 Doubly Linked Lists/dll.js
+++ Section 7 Doubly Linked Lists/dll.js
@@ -125,6 +125,21 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  //Remove method
+  remove(index) {
+    if(index === 0) return this.shift();
+    if(index === this.length - 1) return this.pop();
+    if(index < 0 || index >= this.length) return undefined;
+
+    const temp = this.get(index);
+    temp.prev.next = temp.next;
+    temp.next.prev = temp.prev;
+    temp.next = null;
+    temp.prev = null;
+    this.length--;
+    return temp;
+  }
 }
 
 //Example
