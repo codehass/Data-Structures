6fdfa03ec3e5e90930fe3506e5ca13f209063cbe
hassan <hassan.elouardy06@gmail.com>
2023-06-19 11:47:02 +0100

    Add dequeue method and examples

--- Section 10/queue.js
+++ Section 10/queue.js
@@ -31,4 +31,24 @@ class Queue {
     this.length++;
     return this;
   }
+
+  dequeue() {
+    if(this.length === 0) return undefined;
+    let temp = this.first;
+    if(this.length === 1) {
+      this.first = null;
+      this.last = null;
+    }else {
+      this.first = this.first.next;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
 }
+
+//Example:
+const myQueue = new Queue(7);
+myQueue.enqueue(10);
+myQueue.enqueue(7);
+myQueue.dequeue();
