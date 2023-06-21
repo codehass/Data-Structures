a59b8e10530a44dfca91821edb424e1bb71205d0
hassan <hassan.elouardy06@gmail.com>
2023-06-21 11:02:57 +0100

    Insert method

--- Section 13/trees.js
+++ Section 13/trees.js
@@ -34,4 +34,30 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  //Insert method
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (temp.value === newNode.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
 }
