e6e1d3a76b3ae062edc946c7018ceb789d6a3def
hassan <hassan.elouardy06@gmail.com>
2023-06-21 10:38:16 +0100

    Create the constructor

--- Section 13/trees.js
+++ Section 13/trees.js
@@ -21,3 +21,17 @@
                   -lookup()             v
                   -remove()             v
 */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+}
